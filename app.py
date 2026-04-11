130c7b8dd8bf758719e013f47ab443bd8ab2a3b8
therendev05 <therendev@gmail.com>
2026-04-11 17:20:27 +0200

    Created the logic, the form and the button to remove a task

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from tasks import addNewTask, showTasks
+from tasks import addNewTask, deleteTask, showTasks
 
 app = Flask(__name__)
 
@@ -13,6 +13,13 @@ def home():
     
     currentTasks = showTasks()
     return render_template('home.html', tasks = currentTasks)
+
+# Route to delete a task
+@app.route('/delete/<int:taskId>', methods=['POST'])
+def delete(taskId):
+    deleteTask(taskId)
+    
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tasks.py
+++ tasks.py
@@ -4,5 +4,9 @@ def addNewTask(newTask):
     if newTask and newTask.strip():
         taskList.append(newTask)
 
+def deleteTask(index):
+    if 0 <= index < len(taskList):
+        taskList.pop(index)
+
 def showTasks():
     return taskList
